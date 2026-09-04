6b12bc0088fc2393188944f849b0f7a9b5420ffe
hatchbackkorea-cmyk <hatchbackkorea@gmail.com>
2026-09-05 01:58:38 +0900

    Show minimal RACE profile on launcher screen

--- app/src/main/java/com/seungjae/jangsu280battery/RideCopilotApp.kt
+++ app/src/main/java/com/seungjae/jangsu280battery/RideCopilotApp.kt
@@ -23,6 +23,9 @@ class RideCopilotApp : Application(), Application.ActivityLifecycleCallbacks {
 
     override fun onActivityResumed(activity: Activity) {
         when (activity) {
+            is BikeModeChooserActivity -> activity.window.decorView.post {
+                RaceProfileUiInstaller.install(activity)
+            }
             is MainActivity -> activity.window.decorView.post {
                 installVoiceBoostControl(activity)
                 RideWarningOverlayController.install(activity)
