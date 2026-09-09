3eb93d3118c7d26176cdc63d4a3169942ff5deb5
hatchbackkorea-cmyk <hatchbackkorea@gmail.com>
2026-09-09 20:49:57 +0900

    Fix nearest-tenth boundary expectation

--- app/src/test/java/com/seungjae/jangsu280battery/RaceTimeDisplayTest.kt
+++ app/src/test/java/com/seungjae/jangsu280battery/RaceTimeDisplayTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class RaceTimeDisplayTest {
     @Test fun roundsToNearestTenth() {
         assertEquals("0.0", formatRaceTime(0L))
-        assertEquals("1.2", formatRaceTime(1_149L))
+        assertEquals("1.1", formatRaceTime(1_149L))
         assertEquals("1.2", formatRaceTime(1_150L))
         assertEquals("59.9", formatRaceTime(59_949L))
         assertEquals("1:00.0", formatRaceTime(59_950L))
