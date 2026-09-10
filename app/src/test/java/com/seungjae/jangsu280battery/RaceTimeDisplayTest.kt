0aeb1611a91ae1886db56762f84143f3fc62bdb3
hatchbackkorea-cmyk <hatchbackkorea@gmail.com>
2026-09-10 14:39:45 +0900

    Lock tenth-second half-up rounding behavior

--- app/src/test/java/com/seungjae/jangsu280battery/RaceTimeDisplayTest.kt
+++ app/src/test/java/com/seungjae/jangsu280battery/RaceTimeDisplayTest.kt
@@ -13,4 +13,11 @@ class RaceTimeDisplayTest {
         assertEquals("1:25.1", formatRaceTime(85_149L))
         assertEquals("1:25.2", formatRaceTime(85_150L))
     }
+
+    @Test fun canonicalRoundedMillisecondsAreHalfUpTo100ms() {
+        assertEquals(1_100L, roundRaceTimeMs(1_149L))
+        assertEquals(1_200L, roundRaceTimeMs(1_150L))
+        assertEquals(60_000L, roundRaceTimeMs(59_950L))
+        assertEquals(85_200L, roundRaceTimeMs(85_199L))
+    }
 }
