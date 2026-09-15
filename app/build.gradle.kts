7f186ed9dba11657a4c6e751f7fc1c942dc0c04e
hatchbackkorea-cmyk <hatchbackkorea@gmail.com>
2026-09-15 10:02:32 +0900

    Use current Maven Central WebRTC Android SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,5 +85,5 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.kakao.maps.open:android:2.15.1")
     implementation("com.google.zxing:core:3.5.3")
-    implementation("org.webrtc:google-webrtc:1.0.32006")
+    implementation("io.github.webrtc-sdk:android:150.7871.01")
 }
